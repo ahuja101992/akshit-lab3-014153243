6764ecc05ac7f9905fa75a1884cc1b6ae4a58627
ahuja101992 <ahuja101992@gmail.com>
2019-12-08 20:48:38 -0800

    add section mutation call

--- lab1-014153243-master/frontend/src/mutation/mutation.js
+++ lab1-014153243-master/frontend/src/mutation/mutation.js
@@ -22,22 +22,20 @@ const addBuyerMutation = gql`
     }
 `;
 const addSectionMutation = gql`
-    mutation AddBuyer($first_name: String, $last_name: String, $email_id: String,$rest_zip: String,$rest_name : String, $phone : String){
-        addBuyer(first_name: $first_name, last_name: $last_name, email_id: $email_id, rest_zip :$rest_zip,rest_name:$rest_name, phone:$phone ){
-            first_name,
+    mutation AddSection($email_id: String,$section_name: String){
+        addSection(email_id: $email_id, section_name: $section_name){
             email_id,
-            last_name,
+            section_name
 
         }
     }
 `;
 const addItemMutation = gql`
-    mutation AddBuyer($first_name: String, $last_name: String, $email_id: String,$rest_zip: String,$rest_name : String, $phone : String){
-        addBuyer(first_name: $first_name, last_name: $last_name, email_id: $email_id, rest_zip :$rest_zip,rest_name:$rest_name, phone:$phone ){
-            first_name,
+    mutation AddItem($email_id: String, $dish_name: String, $section_name: String,$dish_desc: String, $dish_price : String){
+        addItem(email_id: $email_id, dish_name: $dish_name, section_name: $section_name, dish_price :$dish_price, dish_desc :$dish_desc ){
             email_id,
-            last_name,
-
+            dish_name,
+            section_name
         }
     }
 `;
